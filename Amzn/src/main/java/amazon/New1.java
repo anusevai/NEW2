0ae24604581e29257dbe2a6a8f6fe8f07a457485
Admin <Admin@DESKTOP-02A10V7>
2023-01-20 07:36:53 +0530

    HELLO

---
+++ Amzn/src/main/java/amazon/New1.java
@@ -0,0 +1,17 @@
+package amazon;
+
+import org.openqa.selenium.WebDriver;
+
+public class New1 {
+	public static WebDriver driver;
+	public void openbrowser()
+	{
+		System.setProperty(null, null));
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Amzn/src/main/java/amazon/program.java
@@ -0,0 +1,17 @@
+package amazon;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class program {
+
+	@Test
+	@BeforeSuite
+	@AfterSuite
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
